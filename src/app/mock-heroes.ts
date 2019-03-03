d07e385fac01cb4e160bd33b331e044ff6b48d34
PriBhula <pridotbhula@gmail.com>
2019-03-03 18:08:34 +1300

    added a loop and more heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Spiderman ', power: 'spider shit'},
+  {id: 11, name: 'Black Panther', power: 'award winning, beautiful'},
+  {id: 12, name: 'Iron Man', power: 'super rich'},
+  {id: 13, name: 'Captain America', power: 'has lots of morals'},
+  {id: 14, name: 'Thor', power: 'literally a norse god'},
+  {id: 15, name: 'Black Widow', power: 'the only woman'},
+  {id: 16, name: 'Ant Man', power: 'funny asf'},
+  {id: 17, name: 'Hulk ', power: 'second best in green (rip gamora)'}
+]

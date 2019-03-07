fe1dbc7640253ff823db2d61156027270d6c813f
PriBhula <pridotbhula@gmail.com>
2019-03-07 13:06:30 +1300

    added click events and details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,14 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    heroes = HEROES;
-    hero: Hero = {
-      id: 1,
-      name: 'Spiderman',
-      power: 'spider shit'
-    };
-    constructor(){}
-    ngOnInit(){
 
-    }
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  constructor() {}
+  ngOnInit() {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+}
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,11 +2,10 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   {id: 1, name: 'Spiderman ', power: 'spider shit'},
-  {id: 11, name: 'Black Panther', power: 'award winning, beautiful'},
-  {id: 12, name: 'Iron Man', power: 'super rich'},
-  {id: 13, name: 'Captain America', power: 'has lots of morals'},
-  {id: 14, name: 'Thor', power: 'literally a norse god'},
-  {id: 15, name: 'Black Widow', power: 'the only woman'},
-  {id: 16, name: 'Ant Man', power: 'funny asf'},
-  {id: 17, name: 'Hulk ', power: 'second best in green (rip gamora)'}
+  {id: 2, name: 'Black Panther', power: 'award winning, beautiful'},
+  {id: 3, name: 'Iron Man', power: 'super rich'},
+  {id: 4, name: 'Captain America', power: 'has lots of morals'},
+  {id: 5, name: 'Thor', power: 'literally a norse god'},
+  {id: 6, name: 'Black Widow', power: 'the only woman'},
+  {id: 7, name: 'Hulk ', power: 'second best in green (rip gamora)'}
 ]
